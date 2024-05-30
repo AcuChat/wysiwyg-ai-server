23cf0d39665212c08583aebd8bf56030b34f1ec9
Michael Wood <michaelwood33311@icloud.com>
2024-05-30 09:23:02 -0400

    added openaiStream function

---
+++ endpoints/openaiStream.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { OPEN_AI_KEY } = process.env;
+const openai = require('../utils/openai');
+
+const openaiStream = async (req, res) => {
+    let { query, model, service } = req.body;
+    if (!query) return res.status(400).json('bad command');
+    if (!service) service = "You are a helpful assistant.";
+    if (!model) model = "gpt-3.5-turbo";
+
+    const messages = openai.initialMessagePair(query, service);
+    openai.openAIGenericChatCompletionRestStream(OPEN_AI_KEY, model, messages, res);
+}
